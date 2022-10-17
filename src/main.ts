c58ab53b7fd4cd22758ed9cb36edf992678f3c63
zaidohDev <danfealmeida@gmail.com>
2022-10-17 12:54:13 -0300

    add update package.json

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT || 3002);
-  console.log(`Running on port ${process.env.PORT || 3002}`);
+  await app.listen(process.env.PORT || 5000);
+  console.log(`Running on port ${process.env.PORT || 5000}`);
 }
 bootstrap();
